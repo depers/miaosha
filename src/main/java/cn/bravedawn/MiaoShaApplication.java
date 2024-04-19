0ddbc4a1fcc75f0ca589b40d51af282b9218d784
depers <dev_fengxiao@163.com>
2024-04-19 22:14:21 +0800

    int project

---
+++ src/main/java/cn/bravedawn/MiaoShaApplication.java
@@ -0,0 +1,21 @@
+package cn.bravedawn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : depers
+ * @program : miaosha
+ * @date : Created in 2024/4/19 17:17
+ */
+
+/**
+ * 启动类
+ */
+@SpringBootApplication
+public class MiaoShaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiaoShaApplication.class, args);
+    }
+}
